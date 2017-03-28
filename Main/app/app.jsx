e74a5a72426720f7901ab224b91fc92e592a7c99
Kelly Douglass <kdouglass@gmail.com>
2017-03-28 16:00:40 -0500

    Wired up React container component

--- Main/app/app.jsx
+++ Main/app/app.jsx
@@ -1,1 +1,16 @@
-console.log('test')
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var Container = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h3>Testing</h3>
+            </div>
+        )
+    }
+})
+
+
+ReactDOM.render(<Container />, document.getElementById('container'))
